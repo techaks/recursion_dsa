adfb099e4af833cf33293f39c5baf03df69daa45
AMRENDRA SINGH <142249591+techaks@users.noreply.github.com>
2026-03-29 16:02:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (89.03%) - LeetSync

---
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+
+        if (s1 == s2)
+            return true;
+
+        for (int i = 0; i < 2; i++) {
+            if(s1[i] != s2[i])
+            swap(s1[i], s1[i + 2]);
+            if (s1 == s2)
+                return true;
+        }
+
+        return false;
+    }
+};
