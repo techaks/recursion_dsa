e94affc86d9de427766707eafed7a2eff5ac03e4
AMRENDRA SINGH <142249591+techaks@users.noreply.github.com>
2026-04-21 00:14:53 +0530

    Time: 8 ms (34.79%) | Memory: 20.1 MB (33.03%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool topologicalSort(unordered_map<int, vector<int>>& adj,
+                         vector<int>& inDegree, int n) {
+        
+        queue<int> que;
+        int count = 0;
+
+        // push all nodes with indegree 0
+        for (int i = 0; i < n; i++) {
+            if (inDegree[i] == 0) {
+                que.push(i);
+            }
+        }
+
+        while (!que.empty()) {
+            int u = que.front();
+            que.pop();
+
+            count++;   
+
+            for (auto v : adj[u]) {
+                inDegree[v]--;
+
+                if (inDegree[v] == 0) {
+                    que.push(v);
+                }
+            }
+        }
+
+        return count == n;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        unordered_map<int, vector<int>> adj;
+        vector<int> inDegree(numCourses, 0);   
+
+        for (auto &vec : prerequisites) {
+            int a = vec[0];
+            int b = vec[1];
+
+            // b -> a
+            adj[b].push_back(a);
+            inDegree[a]++;
+        }
+
+        return topologicalSort(adj, inDegree, numCourses);
+    }
+};
