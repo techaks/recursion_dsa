e3533e7bdcad25ab65a6da7b6ffdb4ab4618aa1a
AMRENDRA SINGH <142249591+techaks@users.noreply.github.com>
2026-04-21 09:48:27 +0530

    Time: 7 ms (45.90%) | Memory: 19.8 MB (40.40%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool topologicalSort(unordered_map<int, vector<int>>& adj,
                          vector<int>& inDegree, int n) {
-        
+
         queue<int> que;
         int count = 0;
 
@@ -17,7 +17,7 @@ public:
             int u = que.front();
             que.pop();
 
-            count++;   
+            count++;
 
             for (auto v : adj[u]) {
                 inDegree[v]--;
@@ -31,20 +31,46 @@ public:
         return count == n;
     }
 
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
-        unordered_map<int, vector<int>> adj;
-        vector<int> inDegree(numCourses, 0);   
+    bool Dfs(unordered_map<int, vector<int>>& adj, int u, vector<bool>& visited,
+             vector<bool>& inRec) {
+        visited[u] = true;
+        inRec[u] = true;
 
-        for (auto &vec : prerequisites) {
+        for (int v : adj[u]) {
+            if (!visited[v] && Dfs(adj, v, visited, inRec)) {
+                return true;
+            } else if (inRec[v] == true)
+                return true;
+        }
+
+        inRec[u] = false;
+
+        return false;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+        unordered_map<int, vector<int>> adj;
+        // vector<int> inDegree(numCourses, 0);
+
+        for (auto& vec : prerequisites) {
             int a = vec[0];
             int b = vec[1];
 
             // b -> a
             adj[b].push_back(a);
-            inDegree[a]++;
+            // inDegree[a]++;
         }
 
-        return topologicalSort(adj, inDegree, numCourses);
+        // return topologicalSort(adj, inDegree, numCourses);
+        vector<bool> visited(numCourses, false);
+        vector<bool> inRec(numCourses, false);
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!visited[i] && Dfs(adj, i, visited, inRec)) {
+                return false;
+            }
+        }
+        return true;
     }
 };
